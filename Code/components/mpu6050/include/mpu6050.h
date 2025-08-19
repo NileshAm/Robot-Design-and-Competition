4b345ff782159f4e371a70dc16b5f11ce9f46f77
NileshAm <nileshamarathunge@gmail.com>
2025-08-19 19:30:37 +0530

    Hardcoded I2C addressed removed.

--- Code/components/mpu6050/include/mpu6050.h
+++ Code/components/mpu6050/include/mpu6050.h
@@ -6,8 +6,6 @@
 extern "C" {
 #endif
 
-#define MPU6050_ADDR_7BIT 0x68  // 0x69 if AD0 is high
-
 esp_err_t mpu6050_init_device(i2c_master_dev_handle_t dev);
 esp_err_t mpu6050_read_accel_gyro_temp(i2c_master_dev_handle_t dev,
                                        float *ax_g, float *ay_g, float *az_g,
